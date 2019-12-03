69689de8a9721de518d36b880d4b1758de4d08b2
rsundar <rohan.s.1987@gmail.com>
2019-12-03 19:14:49 +0530

    Variables, types, and assignment

--- Cards/main.go
+++ Cards/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//var card string = "Ace of spades"
+	card := "Ace of Spades"
+	fmt.Println(card)
+	card = "Five of diamonds"
+	fmt.Println(card)
 }
