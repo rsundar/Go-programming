f2031a246baf3ac7ce9fac4752dfe428abe1ac6b
rsundar <rohan.s.1987@gmail.com>
2019-12-04 23:16:45 +0530

    Added custom type dec and associated reciever for printing out the deck.

--- Cards/main.go
+++ Cards/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Queen of spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
