06f9a95f29093924e1cffd7d3ce2e0a45aa00931
rsundar <rohan.s.1987@gmail.com>
2019-12-05 23:33:47 +0530

    Added newDeck function

--- Cards/main.go
+++ Cards/main.go
@@ -3,13 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	cards := deck{newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Queen of spades")
+	cards := newDeck()
 
 	cards.print()
 	fmt.Println(cards.returnDeck())
-}
-
-func newCard() string {
-	return "Five of diamonds"
 }
