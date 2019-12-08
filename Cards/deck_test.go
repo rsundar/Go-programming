98f878703b9761154bfccbc2538546b8ae8ec758
rsundar <rohan.s.1987@gmail.com>
2019-12-08 23:29:59 +0530

    Added tests for the deck, complete coverage of new deck function.

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,11 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 24 {
 		t.Errorf("Expected deck length of 24, but got %d", len(d))
+	}
+
+	if d[0] != "King of Spades" {
+		t.Errorf("Expected 'King of Spades', but got '%v'", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected 'Four of Clubs', but got '%v'", d[len(d)-1])
 	}
 }
