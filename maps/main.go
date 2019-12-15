91c95219a8094cdfb69bdaf7e726ee70d3b148fb
rsundar <rohan.s.1987@gmail.com>
2019-12-15 23:30:37 +0530

    Operations with maps creation, updation, and deletion

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#74f500",
-	}
+	var colors map[string]string
 
+	newColors := make(map[string]string)
+	// colors := map[string]string{
+	// "red":   "#ff0000",
+	// "green": "#74f500",
+	// }
+
+	newColors["White"] = "#ffffff"
 	fmt.Println(colors)
+	fmt.Println(newColors)
+
+	delete(newColors, "White")
+
+	fmt.Println(newColors)
 }
