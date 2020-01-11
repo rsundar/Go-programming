6bbeb9bb9bd5c72a368b3db8195c0998eaa58628
rsundar <rohan.s.1987@gmail.com>
2020-01-11 23:31:23 +0530

    completed!

--- assignment3/main.go
+++ assignment3/main.go
@@ -3,30 +3,29 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
+	"strings"
 )
-
-type consoleWriter struct{}
 
 func main() {
 
 	// fmt.Println(os.Args)
 	arg1 := os.Args[1]
 
-	data, err := ioutil.ReadFile(arg1)
+	file, err0 := os.Open(arg1)
+	if err0 != nil {
+		fmt.Println("Error: ", err0)
+		os.Exit(1)
+	}
 
-	cw := consoleWriter{}
+	bs := make([]byte, 9999)
+	data, err := file.Read(bs)
+
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	io.Copy(cw, data)
-}
-
-func (consoleWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	fmt.Println("Printed out number of bytes: ", len(bs))
-	return len(bs), error
+	sr := strings.NewReader(string(bs[:data]))
+	io.Copy(os.Stdout, sr)
 }
