f5c8b84d9111f3bd202f6bd561fe1d442909bcca
rsundar <rohan.s.1987@gmail.com>
2020-01-11 23:18:26 +0530

    First attempt at asignment

--- assignment3/main.go
+++ assignment3/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
+type consoleWriter struct{}
+
 func main() {
 
-	fmt.Println(os.Args)
-
+	// fmt.Println(os.Args)
 	arg1 := os.Args[1]
 
-	fmt.Println(arg1)
+	data, err := ioutil.ReadFile(arg1)
+
+	cw := consoleWriter{}
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	io.Copy(cw, data)
+}
+
+func (consoleWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Printed out number of bytes: ", len(bs))
+	return len(bs), error
 }
